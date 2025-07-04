0ff19545d707dad89a1e6f086311bb3ded540298
xiaofei <250079652@qq.com>
2025-07-04 17:58:24 +0800

    add readme

---
+++ src/main/java/leetcode/Leet22.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leet22 {
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> answer = new ArrayList<>();
+        backTrack(n, answer, "", 0, 0);
+        return answer;
+    }
+
+    public static void backTrack(int n, List<String> ans, String cur, int left, int right) {
+        if (cur.length() == 2 * n) {
+            ans.add(cur);
+            return;
+        }
+
+        if (left < n) {
+            cur += "(";
+            backTrack(n, ans, cur, left + 1, right);
+            cur = cur.substring(0, cur.length() - 1);
+        }
+
+        if (right < left) {
+            cur += ")";
+            backTrack(n, ans, cur, left, right + 1);
+            cur = cur.substring(0, cur.length() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
