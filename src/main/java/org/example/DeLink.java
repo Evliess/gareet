ca128fdd9313730a2cefd59b225bdb0a8c71669b
xiaofei <250079652@qq.com>
2025-07-12 18:40:25 +0800

    add readme

--- src/main/java/org/example/DeLink.java
+++ src/main/java/org/example/DeLink.java
@@ -2,174 +2,101 @@ package org.example;
 
 
 // 务必学习Leet146的实现
+class DeNode {
+    DeNode prev;
+    DeNode next;
+    int val;
+
+    DeNode() {
+    }
+
+    DeNode(int val) {
+        this.val = val;
+    }
+}
+
 public class DeLink {
 
-    DeLink prev;
-    DeLink next;
-    DeLink tail;
-    int val;
+    DeNode head;
+    DeNode tail;
     int size;
+    int capacity;
 
-    DeLink(int val) {
-        this.val = val;
-        size++;
+    DeLink(int capacity) {
+        this.capacity = capacity;
+        head = new DeNode();
+        tail = new DeNode();
+        head.next = tail;
+        tail.prev = head;
     }
 
-    public static void print(DeLink l) {
-        if (l == null) return;
-        while (l != null) {
-            System.out.println(l.val);
-            l = l.next;
+
+    public void print() {
+        if (head == null) return;
+        DeNode node = head.next;
+        while (node != null && node != tail) {
+            System.out.println(node.val);
+            node = node.next;
         }
     }
 
-    public static void printForward(DeLink l) {
-        if (l == null) return;
-        DeLink tail = l.tail;
-        while (tail != null) {
-            System.out.println(tail.val);
-            tail = tail.prev;
+    public void printForward() {
+        if (head == null) return;
+        DeNode node = tail.prev;
+        while (node != null && node != head) {
+            System.out.println(node.val);
+            node = node.prev;
         }
     }
 
-    public static DeLink addHead(DeLink l, int val) {
-        if (l == null) return null;
-        DeLink node = new DeLink(val);
-        l.prev = node;
-        node.next = l;
-        node.tail = l.tail;
-        l.tail = null;
-        node.size = l.size;
-        node.size++;
-        return node;
+    public void appendTail(DeNode node) {
+        node.next = tail;
+        node.prev = tail.prev;
+        tail.prev.next = node;
+        tail.prev = node;
+        this.size++;
     }
 
-    public static DeLink deleteByVal(DeLink l, int val) {
-        if (l == null) return null;
-        DeLink curr = l;
-        while (curr != null) {
-            if (curr.val == val) {
-                // if curr is head, then need to update head, tail and size
-                if (curr.prev == null) {
-                    DeLink node = curr.next;
-                    node.tail = curr.tail;
-                    node.prev = null;
-                    node.size = curr.size;
-                    node.size--;
-                    curr.next = null;
-                    return node;
-                }
-                // if curr is tail, then need to update l.tail and size
-                if (curr.next == null) {
-                    DeLink tail = curr.prev;
-                    tail.next = null;
-                    l.tail = tail;
-                    l.size--;
-                    return l;
-                }
-                DeLink prev = curr.prev;
-                DeLink next = curr.next;
-                prev.next = next;
-                next.prev = prev;
-                l.size--;
-                return l;
-            } else {
-                curr = curr.next;
-            }
-        }
-        return l;
+    public void appendHead(DeNode node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+        this.size++;
     }
 
-    public static DeLink deleteByPos(DeLink l, int pos) {
-        if (l == null || pos < 0 || pos > l.size) return l;
-        // delete head
-        if (pos == 0) {
-            DeLink node = l.next;
-            node.tail = l.tail;
-            node.prev = null;
-            node.size = l.size;
-            node.size--;
-            l.next = null;
-            return node;
-        }
-        if (pos == l.size) {
-            DeLink tail = l.tail;
-            l.tail = tail.prev;
-            l.tail.next = null;
-            l.size--;
-            return l;
-        }
-        DeLink target = l;
-        while (pos-- > 0) {
-            target = target.next;
-        }
-        DeLink prev = target.prev;
-        DeLink next = target.next;
-        prev.next = next;
-        next.prev = prev;
-        l.size--;
-        return l;
-
-
+    public void delNode(DeNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
     }
 
-    public static DeLink insert(DeLink l, int pos, int val) {
-        if (l == null || pos < 0) return l;
-        if (pos == 0) return addHead(l, val);
-        if (pos == l.size) return addTail(l, val);
-        DeLink target = l;
-        while (pos > 0) {
-            target = target.next;
-            pos--;
+    public int delTail() {
+        DeNode prev = tail.prev;
+        delNode(prev);
+        this.size--;
+        return prev.val;
+    }
+
+    public void build(int[] nums) {
+        if (nums.length < 1) return;
+        for (int i : nums) {
+            DeNode node = new DeNode(i);
+            appendHead(node);
+            this.size++;
         }
-        DeLink node = new DeLink(val);
-        DeLink tprve = target.prev;
-        tprve.next = node;
-        node.next = target;
-        node.prev = tprve;
-        l.size++;
-        return l;
     }
 
-    public static DeLink addTail(DeLink l, int val) {
-        if (l == null) return null;
-        DeLink node = new DeLink(val);
-        DeLink tail = l.tail;
-        tail.next = node;
-        node.prev = tail;
-        l.tail = node;
-        l.size++;
-        return l;
-    }
-
-
-    public static DeLink build(int[] nums) {
-        if (nums.length == 0) return null;
-        DeLink head = new DeLink(nums[0]);
-        DeLink curr = head;
-        curr.tail = head;
-        for (int i = 1; i < nums.length; i++) {
-            DeLink node = new DeLink(nums[i]);
-            curr.next = node;
-            node.prev = curr;
-            curr = node;
-            if (i == nums.length - 1) {
-                head.tail = node;
-            }
-            head.size++;
-        }
-        return head;
-    }
 
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5};
-        DeLink link = build(nums);
-        link = addHead(link, 0);
-        link = addTail(link, 6);
-        link = insert(link, 7, -1);
-        link = deleteByVal(link, 3);
-        link = deleteByPos(link, 4);
-        print(link);
+        DeLink link = new DeLink(3);
+        link.build(nums);
+//        link.print();
+//        System.out.println("size: " + link.size);
+//        link.appendTail(new DeNode(-5));
+//        link.print();
+        link.appendHead(new DeNode(-1));
+        link.print();
         System.out.println("size: " + link.size);
     }
 
