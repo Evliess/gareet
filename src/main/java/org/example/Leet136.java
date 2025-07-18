0ec67ff5a05fd81f9545adfe324785b873ead18e
xiaofei <250079652@qq.com>
2025-07-18 18:41:19 +0800

    add readme

--- src/main/java/org/example/Leet136.java
+++ src/main/java/org/example/Leet136.java
@@ -3,6 +3,12 @@ package org.example;
 //任何数和其自身做异或运算，结果是 0，即 a⊕a=0。
 //异或运算满足交换律和结合律，即 a⊕b⊕a=b⊕a⊕a=b⊕(a⊕a)=b⊕0=b
 
+
+//位运算： 判断奇偶性 isOdd = num & 1
+//或运算： [设置特定位] data=0b100 data|00001 = 0b101 [合并权限] rw = read|write
+//异或或运算：加解密 data^key^key = data
+//
+
 public class Leet136 {
 
     public int singleNumber(int[] nums) {
