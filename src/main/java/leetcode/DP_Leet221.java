4fb2576343df29cfb60b60f0452fe81155af5a96
xiaofei <250079652@qq.com>
2025-07-26 22:05:50 +0800

    add readme

--- src/main/java/leetcode/DP_Leet221.java
+++ src/main/java/leetcode/DP_Leet221.java
@@ -8,13 +8,23 @@ public class DP_Leet221 {
         int col = matrix[0].length;
         int[][] dp = new int[row][col];
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+            if (matrix[i][0] == '1') {
+                dp[i][0] = 1;
+                //此处容易忘记更新maxSide
+                maxSide = Math.max(maxSide, dp[i][0]);
+            }
+        }
+        for (int j = 0; j < col; j++) {
+            if (matrix[0][j] == '1') {
+                dp[0][j] = 1;
+                //此处容易忘记更新maxSide
+                maxSide = Math.max(maxSide, dp[0][j]);
+            }
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
                 if (matrix[i][j] == '1') {
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
-                    }
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
                 }
                 maxSide = Math.max(maxSide, dp[i][j]);
             }
