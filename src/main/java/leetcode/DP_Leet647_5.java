b93e59fcf95dfd787b52276f2c4a8896dca6355a
xiaofei <250079652@qq.com>
2025-07-27 16:15:51 +0800

    add readme

--- src/main/java/leetcode/DP_Leet647_5.java
+++ src/main/java/leetcode/DP_Leet647_5.java
@@ -2,6 +2,29 @@ package leetcode;
 
 public class DP_Leet647_5 {
 
+    //中心扩展法
+    public static int solution(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += expand(s, i, i);
+            count += expand(s, i, i + 1);
+        }
+        return count;
+    }
+
+    public static int expand(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+
+
     public int countSubstrings(String s) {
         if (s == null || s.isEmpty()) return 0;
         int length = s.length();
@@ -30,5 +53,9 @@ public class DP_Leet647_5 {
             }
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("aaa"));
     }
 }
