a77dcdbaa691718ffe5c82f13ba115ee2783a0c5
xiaofei <250079652@qq.com>
2025-07-27 16:58:52 +0800

    add readme

--- src/main/java/leetcode/Leet438.java
+++ src/main/java/leetcode/Leet438.java
@@ -6,6 +6,27 @@ import java.util.List;
 
 public class Leet438 {
 
+    public List<Integer> solution(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        int[] cntS = new int[26];
+        int[] cntP = new int[26];
+        for (char ch : p.toCharArray()) {
+            cntP[ch - 'a']++;
+        }
+        int lenP = p.length();
+        for (int i = 0; i < lenP; i++) {
+            cntS[s.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(cntP, cntS)) ans.add(0);
+
+        for (int i = lenP; i < s.length(); i++) {
+            cntS[s.charAt(i - lenP) - 'a']--;
+            cntS[s.charAt(i) - 'a']++;
+            if (Arrays.equals(cntP, cntS)) ans.add(i - lenP + 1);
+        }
+        return ans;
+    }
+
     public static List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
         if (s == null || p == null) return ans;
