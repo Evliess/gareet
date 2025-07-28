cdba18fedfd88ea629ff56ff0804a9fb2ec321b1
xiaofei <250079652@qq.com>
2025-07-28 15:00:58 +0800

    add readme

--- src/main/java/leetcode/Leet128.java
+++ src/main/java/leetcode/Leet128.java
@@ -5,6 +5,28 @@ import java.util.Set;
 
 public class Leet128 {
 
+    public int solution(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int max = 0;
+        for (int num : set) {
+            //试想如果num-1存在set中，那么这个序列肯定不是从num开始，所以跳过
+            if (!set.contains(num - 1)) {
+                int curr = num;
+                int len = 1;
+                while (set.contains(curr + 1)) {
+                    curr++;
+                    len++;
+                }
+                max = Math.max(max, len);
+            }
+        }
+        return max;
+    }
+
     public int longestConsecutive(int[] nums) {
         if (nums.length < 2) {
             return nums.length;
@@ -14,9 +36,8 @@ public class Leet128 {
             set.add(num);
         }
         int ans = 0;
-        for (int num : nums) {
+        for (Integer num : set) {
             int x = num;
-            int max;
             while (set.contains(x - 1)) {
                 x--;
             }
