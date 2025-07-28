1db042ec4b5571578982bee169ce6d6a74107160
xiaofei <250079652@qq.com>
2025-07-28 16:44:09 +0800

    add readme

--- src/main/java/leetcode/BT_Leet494.java
+++ src/main/java/leetcode/BT_Leet494.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-public class Leet494 {
+public class BT_Leet494 {
 
     public static int findTargetSumWays(int[] nums, int target) {
         return backTrack(nums, target, 0);
@@ -14,6 +14,22 @@ public class Leet494 {
         int add = backTrack(nums, target - nums[index], index + 1);
         int subtract = backTrack(nums, target + nums[index], index + 1);
         return add + subtract;
+    }
+
+    //下面的写法不推荐，但是方便学习回溯的思路
+    public int backtrack(int[] nums, int target, int current, int index) {
+        if (index == nums.length) {
+            return target == current ? 1 : 0;
+        }
+        current = current + nums[index];
+        int add = backtrack(nums, target, current, index + 1);
+        current = current - nums[index];
+
+        current = current - nums[index];
+        int sub = backtrack(nums, target, current, index + 1);
+        current = current + nums[index]; // 如果实在循环里面，这一步必须写，重置到选择前的状态
+        return add + sub;
+
     }
 
     public static void main(String[] args) {
