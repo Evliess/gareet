ac521e7083d7b606c59296f151bd143464700e14
xiaofei <250079652@qq.com>
2025-07-29 16:13:50 +0800

    add readme

--- src/main/java/leetcode/DP_Leet139.java
+++ src/main/java/leetcode/DP_Leet139.java
@@ -4,21 +4,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DP_Leet139 {
-
+    //dp[i] 表示字符串 s 的前 i 个字符（即 s[0...i-1]）能否被字典中的单词拆分。
     public static boolean wordBreak(String s, List<String> wordDict) {
         if (s == null || s.isEmpty()) return true;
         int len = s.length();
         boolean[] dp = new boolean[len + 1];
-        dp[0] = true;
-        for (int i = 0; i < len; i++) {
+        dp[0] = true; //代表空字符串可以被拆分 dp[1] 代表 s中第一个字符串可以被拆分
+        for (int i = 0; i < dp.length; i++) {
             if (!dp[i]) {
                 continue;
             }
             for (String word : wordDict) {
                 int end = i + word.length();
-                if (end <= len && s.substring(i, i + word.length()).equals(word)) {
-                    dp[i + word.length()] = true;
-                    if (i + word.length() == len) {
+                if (end <= len && s.substring(i, end).equals(word)) {
+                    dp[end] = true;
+                    if (end == len) {
                         return true;
                     }
                 }
