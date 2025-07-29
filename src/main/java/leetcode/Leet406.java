fd4fcf914141cd2bbfac74c12bd0c4d903298270
xiaofei <250079652@qq.com>
2025-07-29 10:50:19 +0800

    add readme

--- src/main/java/leetcode/Leet406.java
+++ src/main/java/leetcode/Leet406.java
@@ -6,8 +6,10 @@ import java.util.List;
 
 public class Leet406 {
 
+    //people[i] = [hi, ki] 表示第 i 个人的身高为 hi ，前面 正好 有 ki 个身高大于或等于 hi 的人。
     public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people, (p1, p2) -> {
+            //根据身高从大到小排序，身高相等，则根据k从小到大排序
             if (p1[0] == p2[0]) return p1[1] - p2[1];
             return p2[0] - p1[0];
         });
