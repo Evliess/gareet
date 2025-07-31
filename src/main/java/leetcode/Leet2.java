de32e98708cbd7b9ff9f308564243700268c19d4
xiaofei <250079652@qq.com>
2025-07-31 22:33:57 +0800

    add readme

--- src/main/java/leetcode/Leet2.java
+++ src/main/java/leetcode/Leet2.java
@@ -2,6 +2,31 @@ package leetcode;
 
 public class Leet2 {
 
+
+    public ListNode solution(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null)
+            return null;
+        int carry = 0;
+        ListNode head = null, tail = null;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry == 1)
+            tail.next = new ListNode(1);
+        return head;
+    }
+
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if (l1 == null || l2 == null) return null;
         ListNode dummy = new ListNode(-1);
