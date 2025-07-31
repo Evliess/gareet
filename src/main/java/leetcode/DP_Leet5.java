5b43e0449b01e5e0c41de765d1eff3096351eab4
xiaofei <250079652@qq.com>
2025-07-31 19:29:58 +0800

    add readme

--- src/main/java/leetcode/DP_Leet5.java
+++ src/main/java/leetcode/DP_Leet5.java
@@ -2,63 +2,47 @@ package leetcode;
 
 public class DP_Leet5 {
     public static String longestPalindrome(String s) {
-        return solution(s);
+        return dp_solution(s);
     }
-
 
     public static String dp_solution(String s) {
-        if (s == null || s.isEmpty()) return s;
-        int n = s.length();
-        boolean[][] flag = new boolean[n][n];
-        int maxLen = 1;
+        if (s == null)
+            return null;
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        int maxlen = 1;
         int start = 0;
-        for (int i = 0; i < n; i++) flag[i][i] = true;
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+            start = i;
+        }
+        for (int i = 0; i < len - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
-                flag[i][i + 1] = true;
+                dp[i][i + 1] = true;
                 start = i;
-                maxLen = 2;
+                maxlen = 2;
             }
         }
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
-                if (s.charAt(i) == s.charAt(j) && flag[i + 1][j - 1]) {
-                    flag[i][j] = true;
-                    if (len > maxLen) {
+        for (int L = 3; L <= len; L++) {
+            for (int i = 0; i < len - L; i++) {
+                int j = i + L - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    if (j - i + 1 > maxlen) {
+                        maxlen = j - i + 1;
                         start = i;
-                        maxLen = len;
                     }
                 }
+
             }
         }
-        return s.substring(start, maxLen);
+        return s.substring(start, start + maxlen);
+
     }
 
-    public static String solution(String s) {
-        if (s == null || s.isEmpty()) return s;
-        int l = 0, r = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expand(s, i, i);
-            int len2 = expand(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > r - l - 1) {
-                l = i - (len - 1) / 2;
-                r = i + len / 2;
-            }
-        }
-        return s.substring(l, r + 1);
-    }
-
-    public static int expand(String s, int l, int r) {
-        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-            l--;
-            r++;
-        }
-        return r - l - 1;
-    }
 
     public static void main(String[] args) {
-        System.out.println(dp_solution("aabbaa"));
+        System.out.println(dp_solution("babad")); //bab
+        System.out.println(dp_solution("cbbd")); //bb
     }
 }
