36e1da3ea8f4b5d0980113f1fc681551add69157
xiaofei <250079652@qq.com>
2025-07-31 14:34:49 +0800

    add readme

--- src/main/java/leetcode/Leet560.java
+++ src/main/java/leetcode/Leet560.java
@@ -4,21 +4,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Leet560 {
-    public static int subarraySum(int[] nums, int k) {
-        calcPrefixSum(nums);
+
+    //最优解
+    public static int solution(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); //初始化index==0 的前缀和，出现次数=1
+        int pre = 0;
         int count = 0;
-        int a;
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) a = 0;
-            else a = nums[i - 1];
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] - a == k) {
-                    count++;
-                    System.out.println(i + " : " + j);
-                }
+        for (int num : nums) {
+            pre = pre + num;
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
+            if (map.containsKey(pre - k)) {
+                count += map.get(pre - k);
             }
         }
         return count;
+    }
+
+
+    public static int subarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return 0;
+        int[] prefixSum = calcPrefixSum(nums);
+        int ans = 0;
+        for (int i = 0; i < prefixSum.length; i++) {
+            for (int j = i; j < prefixSum.length; j++) {
+                if (prefixSum[j] + nums[j] - prefixSum[i] == k) ans++;
+            }
+        }
+        return ans;
     }
 
     public static int solution1(int[] nums, int k) {
@@ -36,31 +49,18 @@ public class Leet560 {
         return count;
     }
 
-    public static int solution(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1); //初始化index==0 的前缀和，出现次数=1
-        int pre = 0;
-        int count = 0;
-        for (int num : nums) {
-            pre = pre + num;
-            if (map.containsKey(pre - k)) {
-                count += map.get(pre - k);
-            }
-            map.put(pre, map.getOrDefault(pre, 0) + 1);
-        }
-        return count;
-    }
 
-
-    public static void calcPrefixSum(int[] nums) {
+    public static int[] calcPrefixSum(int[] nums) {
+        int[] prefixSum = new int[nums.length];
         for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1];
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
         }
+        return prefixSum;
     }
 
 
     public static void main(String[] args) {
-        int[] a = new int[]{1, 1, 1, 2, 3};
-        System.out.println(solution1(a, 3));
+        int[] a = new int[]{1, 1, 1};
+        System.out.println(solution(a, 2));
     }
 }
