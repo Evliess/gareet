a4c7f68b77cdaaab31f81ef2cb2c34077ab133e3
xiaofei <250079652@qq.com>
2025-07-31 17:17:41 +0800

    add readme

--- src/main/java/leetcode/TwoPointers_Leet15.java
+++ src/main/java/leetcode/TwoPointers_Leet15.java
@@ -11,20 +11,20 @@ public class TwoPointers_Leet15 {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i - 1] == nums[i]) continue;
+            if (i > 0 && nums[i - 1] == nums[i]) continue; //i-1的时候已经计算过了，所以跳过
             int left = i + 1;
             int right = nums.length - 1;
             while (left < right) {
-                if (nums[i] > 0) break;
-                int sum = nums[left] + nums[i] + nums[right];
+                if (nums[i] > 0) break; //三个数中最小的大于0，三数之和肯定大于0
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
                     ans.add(Arrays.asList(nums[left], nums[i], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right - 1] == nums[right]) right--;
-                    left++;
-                    right--;
-                } else if (sum < 0) left++;
-                else right--;
+                    while (left < right && nums[left] == nums[left + 1]) left++; //跳重复数
+                    while (left < right && nums[right - 1] == nums[right]) right--; //跳重复数
+                    left++; //左指针右移
+                    right--;//右指针左移
+                } else if (sum < 0) left++; //和小于0移动做指针
+                else right--; //和大于零移动右指针
             }
         }
         return ans;
