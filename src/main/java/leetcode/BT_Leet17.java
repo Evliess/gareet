8c54e241d3fa8e984f09350c58e2ff72310dbc9f
xiaofei <250079652@qq.com>
2025-07-31 16:12:46 +0800

    add readme

--- src/main/java/leetcode/BT_Leet17.java
+++ src/main/java/leetcode/BT_Leet17.java
@@ -1,47 +1,57 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class BT_Leet17 {
 
-    public static List<String> letterCombinations(String digits) {
-        if (digits == null || digits.isEmpty()) return new ArrayList<>();
-        Map<String, String> dict = buildDict();
+    public List<String> letterCombinations(String digits) {
         List<String> ans = new ArrayList<>();
-        backtrack(digits, 0, dict, new StringBuilder(), ans);
+        if (digits == null || digits.isEmpty())
+            return ans;
+        String[] keys = new String[10];
+        keys[0] = "";
+        keys[1] = "abc";
+        keys[2] = "def";
+        keys[3] = "ghi";
+        keys[4] = "jkl";
+        keys[5] = "mno";
+        keys[6] = "pqrs";
+        keys[7] = "tuv";
+        keys[8] = "wxyz";
+        backtrack(digits, keys, "", 0, ans);
         return ans;
     }
 
-    public static void backtrack(String digits, int index, Map<String, String> dict, StringBuilder path, List<String> ans) {
-        if (digits.length() == path.length()) {
-            ans.add(path.toString());
+    /**
+     * @param digits
+     * @param keys
+     * @param curr
+     * @param index  按键的index
+     * @param ans
+     */
+    public void backtrack(String digits, String[] keys, String curr, int index, List<String> ans) {
+        if (curr.length() == digits.length()) {
+            ans.add(curr);
             return;
         }
-        for (char s : dict.get(digits.charAt(index) + "").toCharArray()) {
-            backtrack(digits, index + 1, dict, path.append(s), ans);
-            path.deleteCharAt(path.length() - 1);
+        //根据按键的index找到上面的字符串
+        int pos = Integer.valueOf("" + digits.charAt(index));
+        String posValues = keys[pos - 1];
+        for (int i = 0; i < posValues.length(); i++) {
+            curr = curr + posValues.charAt(i);
+            backtrack(digits, keys, curr, index + 1, ans);
+            curr = curr.substring(0, curr.length() - 1);
         }
-    }
 
-    public static Map<String, String> buildDict() {
-        Map<String, String> dict = new HashMap<>();
-        dict.put("2", "abc");
-        dict.put("3", "def");
-        dict.put("4", "ghi");
-        dict.put("5", "jkl");
-        dict.put("6", "mno");
-        dict.put("7", "pqrs");
-        dict.put("8", "tuv");
-        dict.put("9", "wxyz");
-        return dict;
     }
 
     public static void main(String[] args) {
         List<String> ans = letterCombinations("23");
         System.out.println(ans);
+
+        String str = "123";
+        System.out.println(Integer.valueOf('1' + ""));
     }
 
 
