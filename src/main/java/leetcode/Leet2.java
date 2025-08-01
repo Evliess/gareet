a52d05332564daf6c4b64a3792342b7df468a86f
xiaofei <250079652@qq.com>
2025-08-01 09:23:19 +0800

    add readme

--- src/main/java/leetcode/Leet2.java
+++ src/main/java/leetcode/Leet2.java
@@ -2,8 +2,7 @@ package leetcode;
 
 public class Leet2 {
 
-
-    public ListNode solution(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if (l1 == null && l2 == null)
             return null;
         int carry = 0;
@@ -12,13 +11,13 @@ public class Leet2 {
             int val1 = l1 == null ? 0 : l1.val;
             int val2 = l2 == null ? 0 : l2.val;
             int sum = val1 + val2 + carry;
+            carry = sum / 10;
             if (head == null) {
                 head = tail = new ListNode(sum % 10);
             } else {
                 tail.next = new ListNode(sum % 10);
                 tail = tail.next;
             }
-            carry = sum / 10;
             if (l1 != null) l1 = l1.next;
             if (l2 != null) l2 = l2.next;
         }
@@ -27,64 +26,13 @@ public class Leet2 {
         return head;
     }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null || l2 == null) return null;
-        ListNode dummy = new ListNode(-1);
-        ListNode head = dummy;
-        int shiftLeft = 0;
-        while (l1 != null && l2 != null) {
-            int val = l1.val + l2.val + shiftLeft;
-            if (shiftLeft == 1) shiftLeft = 0;
-            if (val >= 10) {
-                val = val - 10;
-                shiftLeft = 1;
-            }
-            ListNode curr = new ListNode(val);
-            head.next = curr;
-            head = curr;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        while (l1 != null) {
-            int val = l1.val + shiftLeft;
-            if (shiftLeft == 1) shiftLeft = 0;
-            if (val >= 10) {
-                val = val - 10;
-                shiftLeft = 1;
-            }
-            head.next = new ListNode(val);
-            head = head.next;
-            l1 = l1.next;
-        }
-
-        while (l2 != null) {
-            int val = l2.val + shiftLeft;
-            if (shiftLeft == 1) shiftLeft = 0;
-            if (val >= 10) {
-                val = val - 10;
-                shiftLeft = 1;
-            }
-            head.next = new ListNode(val);
-            head = head.next;
-            l2 = l2.next;
-        }
-
-        if (shiftLeft == 1) {
-            head.next = new ListNode(1);
-        }
-        return dummy.next;
-    }
-
     public static void main(String[] args) {
         ListNode head = new ListNode(9);
         head.next = new ListNode(9);
         head.next.next = new ListNode(9);
-
         ListNode head2 = new ListNode(9);
         head2.next = new ListNode(9);
-
         ListNode ans = addTwoNumbers(head, head2);
-
         while (ans != null) {
             System.out.println(ans.val);
             ans = ans.next;
