09972e5f3809a4c8cf2ab756bb93c64b66d25f16
xiaofei <250079652@qq.com>
2025-08-02 11:06:35 +0800

    add readme

--- src/main/java/leetcode/TwoPointers_Leet75.java
+++ src/main/java/leetcode/TwoPointers_Leet75.java
@@ -1,9 +1,34 @@
 package leetcode;
 
 //75. 颜色分类
-public class Leet75 {
+public class TwoPointers_Leet75 {
 
     public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int p0 = 0;
+        int p1 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                //先交换1
+                swap(nums, p1, i);
+                p1++;
+            } else if (nums[i] == 0) {
+                //交换0
+                swap(nums, p0, i);
+                //当0的位置小于1的时候
+                if (p0 < p1) {
+                    //交换1
+                    swap(nums, p1, i);
+                }
+                //同时移动0和1的指针
+                p0++;
+                p1++;
+            }
+        }
+
+    }
+
+    public void solutions1(int[] nums) {
         if (nums == null || nums.length == 0) return;
         int p = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -41,13 +66,11 @@ public class Leet75 {
 
     }
 
-
     public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-
 
     public static void main(String[] args) {
         int[] arr = new int[]{2, 0, 1};
