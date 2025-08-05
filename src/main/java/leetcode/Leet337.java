bd3eb8a29ffc96c2ec737eacc1980ba4c90ed0dd
xiaofei <250079652@qq.com>
2025-08-05 13:36:05 +0800

    add readme

--- src/main/java/leetcode/Leet337.java
+++ src/main/java/leetcode/Leet337.java
@@ -1,57 +1,27 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Leet337 {
+    Map<TreeNode, Integer> selected = new HashMap<>();
+    Map<TreeNode, Integer> noselected = new HashMap<>();
 
-    public static int rob(TreeNode root) {
-        return wrongSolution(root);
-    }
-
-
-    public static int wrongSolution(TreeNode root) {
+    public int rob(TreeNode root) {
         if (root == null) return 0;
-        List<List<Integer>> levels = traverse(root);
-        int odd = 0;
-        int even = 0;
-        for (int i = 0; i < levels.size(); i++) {
-            List<Integer> level = levels.get(i);
-            if ((i & 1) == 1) {
-                for (Integer l : level) {
-                    odd += l;
-                }
-            } else {
-                for (Integer l : level) {
-                    even += l;
-                }
-            }
-        }
-        return Math.max(odd, even);
+        return Math.max(selected.getOrDefault(root, 0), noselected.getOrDefault(root, 0));
     }
 
-    public static List<List<Integer>> traverse(TreeNode root) {
-        if (root == null) return new ArrayList<>();
-        List<List<Integer>> levels = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> list = new ArrayList<>();
-            while (size > 0) {
-                TreeNode node = queue.poll();
-                list.add(node.val);
-                size--;
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-            }
-            levels.add(list);
-        }
-        return levels;
+    public void dfs(TreeNode root) {
+        if (root == null) return;
+        //先看看左右两边，再决定如何选择
+        dfs(root.left);
+        dfs(root.right);
+        //选择
+        selected.put(root, root.val + noselected.getOrDefault(root.left, 0) + noselected.getOrDefault(root.right, 0));
+        //不选择
+        noselected.put(root, Math.max(selected.getOrDefault(root.left, 0), noselected.getOrDefault(root.right, 0)) + Math.max(selected.getOrDefault(root.right, 0), noselected.getOrDefault(root.right, 0)));
     }
-
 
     public static void main(String[] args) {
 
